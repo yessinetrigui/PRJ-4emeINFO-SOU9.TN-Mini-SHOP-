7aa54e4b07d87abedce88350a420e13e96f3942c
yessinetrigui <yessinetrigui03@gmail.com>
2022-05-05 02:25:35 +0100

    update dir

--- Admin/header.php
+++ Admin/header.php
@@ -9,19 +9,19 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link <?php if($EXT=="MainPanel"){echo "active";}  ?>"    aria-current="page" href="/edsa-STI/Projects/Remaster%20SOU9.TN/Admin/MainPanel.php">Home</a>
+            <a class="nav-link <?php if($EXT=="MainPanel"){echo "active";}  ?>"    aria-current="page" href="/edsa-STI/Projects/SOU9.TN/Admin/MainPanel.php">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link  <?php if($EXT=="StorePanel"){echo "active";}  ?>" href="/edsa-STI/Projects/Remaster%20SOU9.TN/Admin/Store/StoreViews/StorePanel.php">Store Manager</a>
+            <a class="nav-link  <?php if($EXT=="StorePanel"){echo "active";}  ?>" href="/edsa-STI/Projects/SOU9.TN/Admin/Store/StoreViews/StorePanel.php">Store Manager</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link  <?php if($EXT=="LivreurPanel"){echo "active";}  ?>" href="/edsa-STI/Projects/Remaster%20SOU9.TN/Admin/Livreur/LivreurViews/LivreurPanel.php">Livreur Manager</a>
+            <a class="nav-link  <?php if($EXT=="LivreurPanel"){echo "active";}  ?>" href="/edsa-STI/Projects/SOU9.TN/Admin/Livreur/LivreurViews/LivreurPanel.php">Livreur Manager</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link  <?php if($EXT=="ClientPanel"){echo "active";}  ?>" href="/edsa-STI/Projects/Remaster%20SOU9.TN/Admin/Client/ClientViews/ClientPanel.php">Client Manager</a>
+            <a class="nav-link  <?php if($EXT=="ClientPanel"){echo "active";}  ?>" href="/edsa-STI/Projects/SOU9.TN/Admin/Client/ClientViews/ClientPanel.php">Client Manager</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link " href="/edsa-STI/Projects/Remaster%20SOU9.TN/logout.php">Logout</a>
+            <a class="nav-link " href="/edsa-STI/Projects/SOU9.TN/logout.php">Logout</a>
           </li>
         </ul>
       </div>
